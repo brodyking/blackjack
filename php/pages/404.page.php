7631d88b20e2053e3644d7d5088acc63d3b5aee4
Brody King <46272894+brodyking@users.noreply.github.com>
2025-08-01 02:45:54 -0500

    Improved 404 Page

--- php/pages/404.page.php
+++ php/pages/404.page.php
@@ -2,14 +2,31 @@
 <html>
 
 <head>
-    <title>Page not Found - Blackjack</title>
-    <link href="/lib/beercss/beer.min.css" rel="stylesheet">
-    <script type="module" src="/lib/beercss/beer.min.js"></script>
-    <link rel="icon" type="image/x-icon" href="/img/logo256.png">
+    <title>404- Blackjack</title>
+    <?php include $snippets["head"]; ?>
 </head>
 
-<body style="max-width: 400px;margin:auto;">
-    Page not found.
+<body style="max-width: 400px;margin:auto;margin-top:50px" class="dark">
+    <?php
+
+    if (isset($_GET["error"])) {
+        echo '<article class="error-container">
+                  <p>' . filter_input(INPUT_GET, "error") . '</p>
+            </article>';
+    }
+
+    ?>
+    <article class="center-align">
+        <h5>Error 404</h5>
+        <p>Page not found</p>
+        <div class="space"></div>
+        <a href="/">
+            <button class="responsive large-elevate">
+                <i>arrow_back</i>
+                <span>Back to safety</span>
+            </button>
+        </a>
+    </article>
 </body>
 
 </html>
