c346854085004e71f4fc4ae889d2853dcd6f9d5a
Brody King <46272894+brodyking@users.noreply.github.com>
2025-08-01 12:10:40 -0500

    Playing page (Relies on JS so basically empty)

---
+++ php/pages/play.page.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Blackjack</title>
+    <?php include $snippets["head"]; ?>
+    <link href="/lib/svg-playing-cards-master/playing-cards.min.css" rel="stylesheet">
+</head>
+
+<body style="max-width: 600px;margin:auto;margin-top:25px" class="dark">
+    <?php
+
+    if (isset($_GET["error"])) {
+        echo '<article class="error-container">
+                  <p>' . filter_input(INPUT_GET, "error") . '</p>
+            </article>';
+    }
+
+    ?>
+
+    <header>
+        <nav>
+            <button class="circle transparent">
+                <a href="/"><i>arrow_back</i></a>
+            </button>
+        </nav>
+    </header>
+    <div id="gameBody">
+    </div>
+    <script src="/js/classes.js"></script>
+    <script src="/js/game.js"></script>
+    <script src="/lib/cardmeister/elements.cardmeister.full.js"></script>
+</body>
+
+</html>
