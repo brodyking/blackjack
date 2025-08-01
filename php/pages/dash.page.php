880d8d8edca44bc55355b2b2fad8b73f3c2c0945
Brody King <46272894+brodyking@users.noreply.github.com>
2025-08-01 02:04:46 -0500

    Moved menu from snippet; Max width changed

--- php/pages/dash.page.php
+++ php/pages/dash.page.php
@@ -6,7 +6,7 @@
     <?php include $snippits["head"]; ?>
 </head>
 
-<body style="max-width: 400px;margin:auto;margin-top:25px" class="dark">
+<body style="max-width: 600px;margin:auto;margin-top:25px" class="dark">
     <?php
 
     if (isset($_GET["error"])) {
@@ -16,8 +16,26 @@
     }
 
     ?>
+    <header>
+        <nav>
+            <button class="circle transparent">
+                <i>menu</i>
+                <menu class="no-wrap right">
+                    <li><a href="/"><i>playing_cards</i>Games</a></li>
+                    <li><a href="/settings"><i>settings</i>Settings</a></li>
+                    <li><a href="/api/usr/logout"><i>door_back</i>Logout</a></li>
+                </menu>
+            </button>
+            <h6 class="max center-align">Blackjack</h6>
+            <button class="circle transparent">
+                <i>code</i>
+                <menu class="no-wrap left">
+                    <li><a href="https://www.github.com/brodyking/blackjack"><i>open_in_new</i>Github</a></li>
+                </menu>
 
-    <?php include $snippits["nav"]; ?>
+            </button>
+        </nav>
+    </header>
     <article class="middle-align center-align">
         <div style="width: 250px;">
             Logged in.
