de1d16c58855556afd0d3d729dfdf3fa95c0a60e
Brody King <46272894+brodyking@users.noreply.github.com>
2025-08-05 00:52:38 -0500

    Playstyle class outline

---
+++ js/classes/playstyle.js
@@ -0,0 +1,7 @@
+export class Playstyle {
+  constructor() {
+    // This will be the players "playstyle" in the future.
+    // All basic strategy will be stored here, and called upon when needed.
+    // Ex: The player's turn will start, and it will call playstyle.basicStrategy(hand)
+  }
+}
